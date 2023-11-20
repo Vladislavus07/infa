5a99494f37fc4a8acec83a24b01999c1d5d2c4f6
Vladislavus07 <143996724+Vladislavus07@users.noreply.github.com>
2023-11-20 11:33:59 +0300

    Add files via upload

---
+++ infa.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+long long itc_len(string str){
+    int length = 0;
+    for (int i = 0; str[i] != '\0'; ++i) {
+        length++;
+    }
+    return length;
+}
+string summa(string a, string b){
+string result;
+int c = a.length();
+int d = b.length();
+int dlina;
+if (d > c) {
+    result = b;
+    dlina = d - c;
+    for (int i = 0; i < dlina; i++) {
+        a = '0' + a;
+    }
+}
