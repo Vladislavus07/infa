20e5060409c7b90bccdad9804ac43704050d94b8
Vladislavus07 <143996724+Vladislavus07@users.noreply.github.com>
2023-11-20 13:02:22 +0300

    Update infa.cpp

--- infa.cpp
+++ infa.cpp
@@ -1,14 +1,26 @@
 #include <iostream>
 #include <string>
-
 using namespace std;
-long long itc_len(string str){
-    int len = 0;
-    for (int i = 0; str[i] != '\0'; ++i) {
-        len++;
-    }
-    return len;
+
+long long itc_len(string str)
+{
+	int a = 0;
+	for (int b = 0; str[b] != '\0'; b++)
+	{
+        	a++;
+	}
+	return a;
 }
+
+string itc_reverse_str(string str)
+{
+    string perev = "";
+    for (int i = itc_len(str) - 1; i >= 0; i--) {
+        perev += str[i];
+    }
+    return perev;
+}
+
 string summ(string st, string st1){
 string res;
 int z = st.size();
@@ -20,6 +32,7 @@ if (f > z) {
     for (int i = 0; i < dlin; i++) {
         st = st + '0';
     }
+    st = itc_reverse_str(st);
     cout << st << endl;
 }
 else if (f < z)
@@ -30,9 +43,12 @@ else if (f < z)
     {
         st1 = st1 + '0';
     }
+    st1 = itc_reverse_str(st1);
+    cout << st1 << endl;
 }
 else{
     res = st;
+    res = itc_reverse_str(res);
 }
 int dlin1 = res.size();
 bool odin = false;
@@ -43,9 +59,22 @@ for (int i = dlin1 - 1; i >= 0; i--) {
     odin = summ / 10;
 }
 
+
 if (odin) {
-    res = '1' + res;
+    res = res + '1';
+}
+//res = itc_reverse_str(res);
+return res;
 }
 
-return res;
+
+
+
+
+int main()
+{
+    string st, st1, res, str;
+    cin >> st >> st1;
+    res = str;
+    cout << summ(st, st1) << endl;
 }
